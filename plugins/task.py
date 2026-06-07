17d212747d23dd04952a890d688a3c9bbed9027a
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-07 10:37:34 +0530

    Update task.py

--- plugins/task.py
+++ plugins/task.py
@@ -1,22 +1,209 @@
-from pyrogram import Client, filters
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
+from database.mongo import tasks
+from datetime import datetime
+import uuid
 
-from utils.states import *
-from database.tasks import *
 
-@Client.on_callback_query(filters.regex("^new_task$"))
-async def new_task(client, query):
+async def create_task(user_id):
 
-    user_id = query.from_user.id
+    task_id = str(uuid.uuid4())
 
-    await create_task(user_id)
+    data = {
 
-    FIRST_LINK[user_id] = True
+        "task_id": task_id,
 
-    await query.message.edit_text(
-        "🔗 Send First Message Link"
+        "user_id": user_id,
+
+        "first_link": None,
+
+        "last_link": None,
+
+        "destination_id": None,
+
+        "destination_title": None,
+
+        "destination_username": None,
+
+        "status": "waiting",
+
+        "total": 0,
+
+        "forwarded": 0,
+
+        "remaining": 0,
+
+        "topics_found": 0,
+
+        "duplicate": 0,
+
+        "deleted": 0,
+
+        "skipped": 0,
+
+        "filtered": 0,
+
+        "percentage": 0,
+
+        "created_at": datetime.utcnow()
+    }
+
+    await tasks.insert_one(data)
+
+    return task_id
+
+
+async def get_task(task_id):
+
+    return await tasks.find_one(
+        {
+            "task_id": task_id
+        }
+    )
+
+
+async def get_user_tasks(user_id):
+
+    return tasks.find(
+        {
+            "user_id": user_id
+        }
+    )
+
+
+async def update_task(
+    task_id,
+    key,
+    value
+):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$set": {
+                key: value
+            }
+        }
+    )
+
+
+async def update_many(
+    task_id,
+    data
+):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$set": data
+        }
+    )
+
+
+async def set_status(
+    task_id,
+    status
+):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$set": {
+                "status": status
+            }
+        }
+    )
+
+
+async def delete_task(task_id):
+
+    await tasks.delete_one(
+        {
+            "task_id": task_id
+        }
+    )
+
+
+async def task_exists(task_id):
+
+    task = await tasks.find_one(
+        {
+            "task_id": task_id
+        }
+    )
+
+    return bool(task)
+
+
+async def increase_forwarded(task_id):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$inc": {
+                "forwarded": 1
+            }
+        }
+    )
+
+
+async def increase_skipped(task_id):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$inc": {
+                "skipped": 1
+            }
+        }
+    )
+
+
+async def increase_deleted(task_id):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$inc": {
+                "deleted": 1
+            }
+        }
+    )
+
+
+async def increase_duplicate(task_id):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$inc": {
+                "duplicate": 1
+            }
+        }
+    )
+
+
+async def increase_filtered(task_id):
+
+    await tasks.update_one(
+        {
+            "task_id": task_id
+        },
+        {
+            "$inc": {
+                "filtered": 1
+            }
+        }
     )
