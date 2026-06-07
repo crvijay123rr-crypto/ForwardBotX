f35ef4e080d686e277ace8da383fce12b4050d38
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-07 10:05:37 +0530

    Initialize bot.py with basic bot setup

---
+++ bot.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+from config import *
+
+app = Client(
+    "ForwardBotX",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
+
+print("Bot Started")
+
+app.run()
