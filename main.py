9ca900f4977fcd61b18c116f37e49b3e438ff9b3
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-12 11:37:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from pyrogram import Client
+from config import *
+
+app = Client(
+    "ForwardBot",
+
+    api_id=API_ID,
+
+    api_hash=API_HASH,
+
+    bot_token=BOT_TOKEN,
+
+    plugins=dict(
+        root="plugins"
+    )
+)
+
+
+if __name__ == "__main__":
+
+    print(
+        "\n"
+        "━━━━━━━━━━━━━━━━━━━━━━\n"
+        "🚀 FORWARD BOT STARTED\n"
+        "━━━━━━━━━━━━━━━━━━━━━━\n"
+    )
+
+    app.run()
